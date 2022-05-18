1e117af93b749c69225a5eb1b11c6f22171ba536
Evan Mei <em@Evans-MacBook-Pro.local>
2022-05-17 22:09:45 -0400

    email popup

--- portfolio/src/Components/contact.js
+++ portfolio/src/Components/contact.js
@@ -7,8 +7,8 @@ export default function Contact(props) {
     <div ref={contactRef} class="contactArea">
     <div class="contactDiv">
         <h1 id="contactTitle">Lets Connect!</h1>
-        <h2>Email: em8258580@gmail.com</h2>
-        <h2>Resume:<a href={Resume} target="_blank" rel="noopener noreferrer"> Click here!</a></h2>
+        <h2>Email: <a href="mailto:em8258580@gmail.com" target="_blank" rel="noopener noreferrer"> em8258580@gmail.com</a></h2>
+        <h2>Resume: <a href={Resume} target="_blank" rel="noopener noreferrer"> Click here!</a></h2>
       </div>
   </div>
     );

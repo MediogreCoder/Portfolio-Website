26727d5738458b2fe4f6659e7dee8006280c2053
Evan Mei <em@Evans-MacBook-Pro.local>
2022-05-18 14:29:52 -0400

    readded icon scale and

--- portfolio/src/Components/about.js
+++ portfolio/src/Components/about.js
@@ -21,19 +21,19 @@ export default function About(props) {
       <div class="skillsDiv">
         <h1>My Tools</h1>
         <div class='skillsBox'>
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt='sl'/>
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt='sl' />
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" alt='sl'/>
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" alt='sl'/>
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" alt='sl'/>
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" alt='sl'/>
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" alt='sl'/>
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg" alt='sl'/>
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" alt='sl'/>
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain.svg" alt='sl'/>
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt='sl'/>
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt='sl'/>
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" alt='sl'/>
+        <img class='skillLogo' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt='sl'/>
+        <img class='skillLogo' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt='sl' />
+        <img class='skillLogo'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" alt='sl'/>
+        <img class='skillLogo'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" alt='sl'/>
+        <img class='skillLogo'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" alt='sl'/>
+        <img class='skillLogo'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" alt='sl'/>
+        <img class='skillLogo'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" alt='sl'/>
+        <img class='skillLogo'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg" alt='sl'/>
+        <img class='skillLogo'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" alt='sl'/>
+        <img class='skillLogo'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain.svg" alt='sl'/>
+        <img class='skillLogo'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt='sl'/>
+        <img class='skillLogo' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt='sl'/>
+        <img class='skillLogo'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" alt='sl'/>
         </div>
       </div>
     </div>
